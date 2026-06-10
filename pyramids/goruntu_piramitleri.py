dee20185ec218be8d8ea678ba1e33683b284b2c1
huseyinanbar <huseyinanbar3@gmail.com>
2026-06-10 19:53:16 +0300

    Update goruntu_piramitleri.py

--- pyramids/goruntu_piramitleri.py
+++ pyramids/goruntu_piramitleri.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
 
-resim = cv2.imread("emelsayin.png",0)
+resim = cv2.imread("resim1.png",0)
 
 iki_kat_buyuk = cv2.pyrUp(resim)
 iki_kat_kucuk = cv2.pyrDown(resim)
 
-cv2.imshow("emelsayin", resim)
+cv2.imshow("resim", resim)
 cv2.imshow("iki kat buyuk", iki_kat_buyuk)
 cv2.imshow("iki kat kucuk", iki_kat_kucuk)
 
